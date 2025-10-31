dcae6cb78174a46ee6d5240d3862dc1490bebfa4
shrey3456 <shrey2859@gmail.com>
2025-11-01 00:58:40 +0530

    some minor chages

--- backend/config/mongoDb.js
+++ backend/config/mongoDb.js
@@ -10,7 +10,7 @@ const connectMongoDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log("MongoDB Connected Successfully!");
+        console.log("MongoDB Connected Successfully! data stored in database ");
     } catch (error) {
         console.error("MongoDB Connection Failed:", error);
         process.exit(1);
